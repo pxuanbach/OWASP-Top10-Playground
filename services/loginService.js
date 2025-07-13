e6946990af4c94fc17c7cd0093bc53ed414c2e2b
Toàn <khanhtoannguyen12@gmail.com>
2025-07-13 21:47:14 +0700

    owasp-7-8-9-10

--- services/loginService.js
+++ services/loginService.js
@@ -1,9 +1,24 @@
 // services/loginService.js
+// Fake user database
+const USERS = {
+  admin: 'password123'
+};
 
 function handleLogin(req, res) {
     const { username, password } = req.body;
     console.log('Login attempt:', { username, password });
-    res.send('Login received!');
+    if (USERS[username] && USERS[username] === password) {
+    const sessionId = `${username}-fixedsessionid`;
+
+    res.cookie('session', sessionId);
+    return res.redirect('/dashboard');
+  }
+
+  res.send(`
+    <p style="color:red">Invalid login! Try again.</p>
+    <a href="/login">Back</a>
+  `);
+
 }
 
 module.exports = { handleLogin };
